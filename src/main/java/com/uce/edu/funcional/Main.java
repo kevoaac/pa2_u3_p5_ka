171d70fc5bd21a050abcbef30b240db9e311725a
kevoaac <130163890+kevoaac@users.noreply.github.com>
2024-02-18 22:25:30 -0500

    TALLER 33: Lambdas y Métodos referenciados

--- src/main/java/com/uce/edu/funcional/Main.java
+++ src/main/java/com/uce/edu/funcional/Main.java
@@ -24,7 +24,7 @@ public class Main {
         };
         System.out.println(supplierLambda.getId());
 
-        IPersonaSupplier<String> supplierLambda2 = () -> "466456563"+"pais";
+        IPersonaSupplier<String> supplierLambda2 = () -> "466456563" + "pais";
         System.out.println(supplierLambda2.getId());
 
         IPersonaSupplier<Integer> supplierLambda3 = () -> {
@@ -35,9 +35,14 @@ public class Main {
         System.out.println(supplierLambda3.getId());
 
         // Métodos referenciados
+        // métodos que no importa que no implemente la interfaz, basta con que tengan un método que CUMPLA LA INTERFAZ
+        System.out.println(" >Métodos Referenciados");
+        MetodosReferenciados met = new MetodosReferenciados();
+        IPersonaSupplier<String> supplierLambda4 = met::obtenerId;
+        System.out.println(supplierLambda4.getId()); // usa getId y no obtenerId() porque lo que se pasa es el cuerpo del metodo
 
         //  2. Consummer -----------------------------------------------------
-        System.out.println(">>CONSUMER");
+        System.out.println("\n>>CONSUMER");
         //  Clases
         System.out.println(" >Clases");
 
@@ -48,14 +53,19 @@ public class Main {
         System.out.println(" >Lambdas");
 
         IPersonaConsumer<String> consummerLambda = (cadenaStr) -> {
-            System.out.println("Actualizando: " + cadenaStr );
+            System.out.println("Actualizando: " + cadenaStr);
             System.out.println(cadenaStr + "Actializado");
         };
         consummerLambda.accept("Alumno");
 
+        // Métodos referenciados
+        System.out.println(" >Métodos Referenciados");
 
-        //  2. Consummer ------------------------------------------------------
-        System.out.println(">>PREDICATE");
+        IPersonaConsumer<String> consumer2 = met::procesar;
+        consumer2.accept("Kevin Referenciado");
+
+        //  3. Predicate ------------------------------------------------------
+        System.out.println("\n>>PREDICATE");
 
         //  Lambdas
         System.out.println(" >Lambdas");
@@ -65,7 +75,74 @@ public class Main {
         System.out.println("El valor es igual: " + esIgual);
 
         IPersonaPredicate<String> predicateLambda2 = letra -> "Kevin".contains(letra);
-        System.out.println( "Contienen letra: " + predicateLambda2.evaluar("z"));
+        System.out.println("Contienen letra: " + predicateLambda2.evaluar("z"));
 
+        // Métodos referenciados
+        System.out.println(" >Métodos Referenciados");
+
+        IPersonaPredicate<String> predicate2= met::evaluar;
+        System.out.println(predicate2.evaluar("X"));
+
+        IPersonaPredicate<Integer> predicate3= met::evaluar;
+        System.out.println(predicate3.evaluar(8));
+
+        //  4. Function ------------------------------------------------------
+        System.out.println("\n>>FUNCTION");
+
+        //  Lambdas
+        System.out.println(" >Lambdas");
+        //<TipoDeRetorno, TipoDeParametro>
+        IPersonaFunction<String, Integer> funcionLambda = numero -> {
+            numero = numero + Integer.valueOf(10);
+            String numeroString = numero.toString().concat("-Valor");
+            return numeroString;
+        };
+
+        System.out.println(funcionLambda.aplicar(7));
+
+        IPersonaFunction<Empleado, Ciudadano> funcionLambda1 = ciudadano -> {
+            Empleado empl = new Empleado();
+            empl.setNombreCompleto(ciudadano.getNombre() + ciudadano.getApellido());
+            if (ciudadano.getProvincia().compareTo("pichincha") == 0) {
+                empl.setPais("Ecuador");
+            }
+            return empl;
+        };
+        Ciudadano ciud = new Ciudadano();
+        ciud.setApellido("Andrade");
+        ciud.setNombre("Kevin");
+        ciud.setProvincia("pichincha");
+
+        Empleado empl = funcionLambda1.aplicar(ciud);
+
+        System.out.println(empl);
+
+        // Métodos referenciados
+        System.out.println(" >Métodos Referenciados");
+        Empleado emp2 = new Empleado();
+        emp2.setNombreCompleto("Daniel Tobar");
+        emp2.setPais("Ecuador");
+
+        IPersonaFunction<Ciudadano, Empleado> function2 = met::cambiar;
+        System.out.println(function2.aplicar(emp2));
+
+        //  5. Unary Operator ------------------------------------------------------
+        System.out.println("\n>>UNARY OPERATOR");
+
+        //  Lambdas
+        System.out.println(" >Lambdas");
+        IPersonaUnaryOperator<Integer> unaryOperatorLambda = numero -> numero + (numero * 2);
+
+        System.out.println(unaryOperatorLambda.aplicar(14));
+        // Extendiendo de Function
+
+        IPersonaUnaryOperatorFunction<Integer> unaryOperatorLambda2 = numero -> numero + (numero * 2);
+        System.out.println(unaryOperatorLambda2.aplicar(14));
+
+        // Métodos referenciados
+        System.out.println(" >Métodos Referenciados");
+        IPersonaUnaryOperatorFunction<Empleado> unary3 = met::procesar;
+        Empleado empl3 = unary3.aplicar(emp2);
+        System.out.println(empl3);
     }
 }
---
+++ src/main/java/com/uce/edu/funcional/MetodosReferenciados.java
@@ -0,0 +1,37 @@
+package com.uce.edu.funcional;
+
+public class MetodosReferenciados {
+    public String obtenerId() {
+        String cedula = "44345435345";
+        cedula = cedula + "Referenciado";
+        return cedula;
+    }
+
+    public void procesar(String cadena){
+        System.out.println(cadena);
+    }
+
+    public boolean evaluar(String cadena){
+        return "Kevin".contains(cadena);
+    }
+    public boolean evaluar(Integer numero){
+        return 8 ==numero;
+    }
+
+    public Ciudadano cambiar(Empleado empleado){
+        Ciudadano ciud = new Ciudadano();
+        String nombre = empleado.getNombreCompleto().split(" " )[0];
+        String apellido = empleado.getNombreCompleto().split(" " )[1];
+        ciud.setNombre(nombre);
+        ciud.setApellido(apellido);
+        ciud.setProvincia("chimborazo");
+
+        return ciud;
+    }
+
+    public Empleado procesar(Empleado empl){
+        empl.setNombreCompleto(empl.getNombreCompleto() + " " + empl.getPais());
+        empl.setPais(empl.getPais() + " de nacimiento");
+        return empl;
+    }
+}
